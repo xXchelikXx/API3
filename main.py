0ec6689584fe0a74750164399ffee45082c07d54
xXchelikXx <162018732+xXchelikXx@users.noreply.github.com>
2024-05-05 14:38:45 +0300

    оформил код main.py

--- main.py
+++ main.py
@@ -4,23 +4,18 @@ import argparse
 from urllib.parse import urlparse
 from dotenv import load_dotenv
 
+
 def is_bitlink(bitlink, bitly_token):
     url = f'https://api-ssl.bitly.com/v4/bitlinks/{bitlink}'
-    headers = {
-        "Authorization": f"Bearer {bitly_token}"
-    }
+    headers = {"Authorization": f"Bearer {bitly_token}"}
     response = requests.get(url, headers=headers)
     return response.ok
 
 
 def shorten_link(bitly_token, long_url):
     url = 'https://api-ssl.bitly.com/v4/shorten'
-    headers = {
-        "Authorization": f"Bearer {bitly_token}"
-    }
-    params = {
-        'long_url':long_url
-    }
+    headers = {"Authorization": f"Bearer {bitly_token}"}
+    params = {'long_url': long_url}
     response = requests.post(url, headers=headers, json=params)
     response.raise_for_status()
     return response.json()['id']
@@ -28,23 +23,19 @@ def shorten_link(bitly_token, long_url):
 
 def count_clicks(bitlink, bitly_token):
     url = f'https://api-ssl.bitly.com/v4/bitlinks/{bitlink}/clicks/summary'
-    headers = {
-        "Authorization": f"Bearer {bitly_token}"
-    }
-    params = {
-        'unit':'month',
-        'units':'-1'
-    }
+    headers = {"Authorization": f"Bearer {bitly_token}"}
+    params = {'unit': 'month', 'units': '-1'}
     response = requests.get(url, headers=headers, params=params)
     response.raise_for_status()
     return response.json()['total_clicks']
 
 
 def main():
-    
     load_dotenv()
     bitly_token = os.environ['BITLY_TOKEN']
-    parser = argparse.ArgumentParser(description='Сокращает ссылку, показывает клики по сокращённой ссылке')
+    parser = argparse.ArgumentParser(
+        description='Сокращает ссылку, показывает клики по сокращённой ссылке'
+    )
     parser.add_argument('--url', type=str, help='Введите ссылку')
     short_link = parser.parse_args()
     parsed_url = urlparse(short_link.url)
